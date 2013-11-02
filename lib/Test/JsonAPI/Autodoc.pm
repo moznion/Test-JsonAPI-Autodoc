392c5121d923091db9bf19a0e6e71dac951edc12
moznion <moznion@gmail.com>
2013-11-02 23:44:03 +0900

    Checking in changes prior to tagging of version 0.03.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index bd5f51b..08d71a6 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,7 +2,7 @@ Revision history for Perl extension Test-JsonAPI-Autodoc
    
     {{$NEXT}}
    
    -0.02 2013-11-02T10:11:30Z
    +0.03 2013-11-02T14:43:51Z
    
         - Add documents.
         - Fix some bugs.

--- lib/Test/JsonAPI/Autodoc.pm
+++ lib/Test/JsonAPI/Autodoc.pm
@@ -13,7 +13,7 @@ use Test::JsonAPI::Autodoc::Markdown;
 
 our @EXPORT = qw/describe http_ok set_documents_path set_template/;
 
-our $VERSION = "0.02";
+our $VERSION = "0.03";
 
 my $in_describe;
 my $results;
